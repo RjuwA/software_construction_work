b0957cc566c734243779e93c79e6e6ec51aee185
Retired <3170174884@qq.com>
2024-03-04 18:41:16 +0800

    assignment1

---
+++ assignment1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请分别输入两个数字和一个运算符");
+            float num1 = int.Parse (Console.ReadLine());
+            float num2 = int.Parse (Console.ReadLine());
+            char  c = char.Parse (Console.ReadLine());
+            float s;
+            switch (c)
+            {
+                case '+': 
+                    s = num1 + num2;
+                    Console.WriteLine("result="+ s);
+                    break;
+                case '-': 
+                    s = num1 - num2;
+                    Console.WriteLine("result=" + s);
+                    break;
+                case '*': 
+                    s = num1 * num2;
+                    Console.WriteLine("result=" + s);
+                    break;
+                case '/':
+                    if (num2 == 0) Console.WriteLine("input error");
+                    else { s = num1 / num2; Console.WriteLine("result=" + s); }
+                    break;
+                default:Console.WriteLine("input error");break;
+            }
+
+        }
+    }
+}
---
+++ assignment1/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float num1 = float.Parse(textBox1.Text);
+            float num2 = float.Parse(textBox3.Text);
+            char c= ' ';
+            if (radioButton1.Checked)   c = char.Parse(radioButton1.Text);
+            if (radioButton2.Checked)   c = char.Parse(radioButton2.Text);
+            if (radioButton3.Checked)   c = char.Parse(radioButton3.Text);
+            if (radioButton4.Checked)   c = char.Parse(radioButton4.Text);
+            float s=0;
+            bool flag=true;
+            switch (c)
+            {
+                case '+':s = num1 + num2;break;
+                case '-':s = num1 - num2;break;
+                case '*':s = num1 * num2;break;
+                case '/':
+                    if (num2 == 0) flag=false;
+                    else s = num1 / num2; 
+                    break;
+                default: flag=false; break;
+            }
+            string str = s.ToString("0.00");
+            if (flag) textBox4.Text = str;
+            else textBox4.Text = "input error";
+        }
+    }
+}
