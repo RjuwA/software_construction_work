cbedcba7a1ae7a9f4da088ce63f16319bb581787
Retired <3170174884@qq.com>
2024-03-06 21:00:39 +0800

    assignment2

---
+++ assignment2/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int[] num= new int[n]; 
+            for(int i=2;i*i<=n;i++)
+            {
+                if(num[i]==0)
+                {
+                    for(int j=2*i;j<n;j=j+i)
+                    {
+                        num[j] = 1;
+                    }
+                }
+            }
+            Console.WriteLine("2~100的所有素数为：");
+            for(int k=2;k<n;k++)
+            {
+                if (num[k] == 0)
+                {
+                    Console.Write(k + " ");
+                }
+            }
+
+
+        }
+    }
+}
