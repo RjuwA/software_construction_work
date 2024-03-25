dbc3bb79da716e7030fda5edf5edddddcf9e43f0
Retired <3170174884@qq.com>
2024-03-25 13:58:38 +0800

    assignment4

---
+++ assignment4/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.ForEach(Console.WriteLine);
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+            list.ForEach(data => max = data > max ? data : max);
+            list.ForEach(data => min = data < min ? data : min);
+            list.ForEach(data => sum+=data);
+            Console.WriteLine($"max={max},min={min},sum={sum}");
+
+        }
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+
+        public class LinkedList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public LinkedList()
+            {
+                tail = head = null;
+            }
+            public Node<T> Head { get => head; }
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if(tail==null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                    
+                }
+            }
+
+            public void ForEach(Action<T> action)
+            {
+                Node<T> n = head;
+                while (n != null)
+                {
+                    action(n.Data);
+                    n = n.Next;
+                }
+            }
+            
+        }
+    }
+}
---
+++ assignment4/ConsoleApp2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubForm clockform1 = new SubForm();
+            clockform1.clock.SetTime();  //设置闹钟时间
+            clockform1.clock.Countdown(clockform1.clock.Count);
+        }
+    }
+
+    public delegate void ClockEvent(object sender);
+
+    public class Clock
+    {
+        public event ClockEvent Tick;
+        public event ClockEvent Alarm;
+        private int hour,minute,second;
+        private int count;
+        public void SetTime()
+        {
+            try
+            {
+                Console.WriteLine("请设置闹钟的时分秒");
+
+                Console.WriteLine("时:");
+                hour = int.Parse(Console.ReadLine());
+                if (hour < 0 || hour > 23)
+                {
+                    throw new ArgumentException("时应在0到23之间");
+                }
+
+                Console.WriteLine("分:");
+                minute = int.Parse(Console.ReadLine());
+                if (minute < 0 || minute > 59)
+                {
+                    throw new ArgumentException("分应在0到59之间");
+                }
+
+                Console.WriteLine("秒:");
+                second = int.Parse(Console.ReadLine());
+                if (second < 0 || second > 59)
+                {
+                    throw new ArgumentException("秒应在0到59之间");
+                }
+
+                count = hour * 3600 + minute * 60 + second;
+                Console.WriteLine($"你输入的闹钟为{hour:00}:{minute:00}:{second:00}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("输入错误，请输入数字");
+                SetTime(); 
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("错误: " + ex.Message);
+                SetTime(); 
+            }
+        }
+        public int Count 
+        {
+            get { return count; }
+            set { count = value; }
+        }
+        public void Countdown(int count)
+        {       
+            for(int i = count;i >= 0;i--)
+            {
+                if (i == 0)
+                {
+                    Alarm(this);
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    Tick(this); 
+                }
+            }
+        }
+
+
+
+
+    }
+
+    public class SubForm
+    {
+        public Clock clock = new Clock();
+        public SubForm()
+        {
+            clock.Alarm += new ClockEvent(OnAlarm);
+            clock.Tick += new ClockEvent(OnTick);
+        }
+        void OnAlarm(object sender)
+        {
+            Console.WriteLine("Alarm!");
+        }
+        void OnTick(object sender)
+        {
+            Console.WriteLine("Tick!");
+        }
+    }
+}
